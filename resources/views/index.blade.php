19d392c55ddf05574d2f4a2fb591fe9bbfa8f9d3
Asif Sharif Shahid <asifsharif190@gmail.com>
2024-10-18 19:56:29 +0500

    change homepage title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Asif Sharif Shahid</title>
+        <title>Asif Sharif Shahid - Full Stack Engineer</title>
 
+        
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
